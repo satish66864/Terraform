6b5668e7f2572853bcbab058adabc44ac5a96746
root <root@ip-10-0-0-173.ap-south-1.compute.internal>
2023-10-03 13:41:58 +0000

    this is modified terraform code file with block storage and tags

--- provider.tf
+++ provider.tf
@@ -6,10 +6,16 @@ secret_key = "qlQd4JolnkiPJGZKCno9q6MILAkULKIqLquRuABn"
 
 resource "aws_instance" "key" {
 ami = "ami-03d294e37a4820c21"
+key_name = "mykey.pem"
 instance_type = "t2.micro"
 availability_zone = "ap-south-1a"
 tags = {
 Name = "rapo-server"
+environment = "dev"
+application = "swiggy"
+}
+root_block_device {
+volume_size = 20
 }
 }
 
