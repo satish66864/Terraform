2b6c40ede068547852e47282eeeedc27a8bdd2d5
root <root@ip-10-0-0-173.ap-south-1.compute.internal>
2023-10-03 14:25:13 +0000

    terraform code file for creating two servers in different regions by using single .tf file

---
+++ two-servers.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "one" {
+  ami               = ""
+  key_name          = "mykey.pem"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name        = "first_server"
+    environment = "test"
+    application = "rapido"
+  }
+  root_block_device {
+    volume_size = 20
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "two" {
+  ami               = ""
+  key_name          = "mykey.pem"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name        = "second_server"
+    environment = "dev"
+    application = "swiggy"
+  }
+  root_block_device {
+    volume_size = 15
+  }
+}
