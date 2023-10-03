adfc4dbd2b03a170925c6e8937b2ab481898a3c4
root <root@ip-10-0-0-173.ap-south-1.compute.internal>
2023-10-03 13:53:52 +0000

    updated with indentation

--- provider.tf
+++ provider.tf
@@ -1,21 +1,21 @@
 provider "aws" {
-region = "ap-south-1"
-access_key = "AKIA3QCEQ6YF733OTRV7"
-secret_key = "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"
+  region     = "ap-south-1"
+  access_key = "AKIA3QCEQ6YF733OTRV7"
+  secret_key = "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"
 }
 
 resource "aws_instance" "key" {
-ami = "ami-03d294e37a4820c21"
-key_name = "mykey.pem"
-instance_type = "t2.micro"
-availability_zone = "ap-south-1a"
-tags = {
-Name = "rapo-server"
-environment = "dev"
-application = "swiggy"
-}
-root_block_device {
-volume_size = 20
-}
+  ami               = "ami-03d294e37a4820c21"
+  key_name          = "mykey.pem"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name        = "rapo-server"
+    environment = "dev"
+    application = "swiggy"
+  }
+  root_block_device {
+    volume_size = 20
+  }
 }
 
